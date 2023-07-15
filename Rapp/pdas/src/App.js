fd14b0b5247255e7fcae30c32d6c425ceeb21253
Benson <benzps01@gmail.com>
2023-07-15 23:36:51 +0530

    added profile card details and its css

--- Rapp/pdas/src/App.js
+++ Rapp/pdas/src/App.js
@@ -1,12 +1,48 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      Personal Digital Assistants
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns is-centered">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imageSrc={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imageSrc={CortanaImage}
+                description="Cortana was mabe by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                imageSrc={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
