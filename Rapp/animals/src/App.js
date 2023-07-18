7e881630426bbea5ada4e4d14f5e1b3709d45c48
Benson <benzps01@gmail.com>
2023-07-18 23:31:10 +0530

    added feature to show all animal list

--- Rapp/animals/src/App.js
+++ Rapp/animals/src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./App.css";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log("Buton was clicked!");
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
