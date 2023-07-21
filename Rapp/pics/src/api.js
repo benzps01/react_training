cf06453815d00be4d50f414f261d60ced91b9e77
Benson <benzps01@gmail.com>
2023-07-21 21:32:31 +0530

    api file to call images from upslash api

--- Rapp/pics/src/api.js
+++ Rapp/pics/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const searchImages = async ({ searchterm }) => {
+const searchImages = async (searchterm) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID hdsLFoKBSs2cMjlbo1lV3BGC6sGLnOEnomZgtvwA5mA",
