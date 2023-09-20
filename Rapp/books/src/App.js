914c69ad79f0bb8b237e6277052bba8876f5ea9d
benzps01 <benzps01@gmail.com>
2023-09-20 19:12:03 +0530

    add random ID generation

--- Rapp/books/src/App.js
+++ Rapp/books/src/App.js
@@ -5,13 +5,17 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title: title }];
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ]; //since key and value have same name we can condense that
 
     setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
