cf9b928f16beaee57a2ffa73cbdafb6f49d3ee53
benzps01 <benzps01@gmail.com>
2023-09-28 19:36:55 +0530

    added count state which increments when clicked

--- Rapp/books/src/context/books.js
+++ Rapp/books/src/context/books.js
@@ -1,5 +1,22 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
-export default BooksContext;
+function Provider({ children }) {
+  const [count, setCount] = useState(5);
+
+  const valueToShare = {
+    count,
+    incrementCount: () => {
+      setCount(count + 1);
+    },
+  };
+  return (
+    <BooksContext.Provider value={valueToShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
+
+export { Provider }; //named export
+export default BooksContext; //default export
